9f79e423c06c4b51a9d01bde6f692d1028edb424
Michael <admin@fillthegaplearning.co.uk>
2020-10-17 12:45:16 +0100

    Initial commit, two tasks

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,10 @@
 # WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(self):
+    # to check what is returned
+    # print(self["name"])
+    # self["name"] returns the shop name because it is the only 'name' on this layer of the program.because
+    return self["name"]
+    
+def get_total_cash(self):
+    
+    return self["admin"]["total_cash"]
