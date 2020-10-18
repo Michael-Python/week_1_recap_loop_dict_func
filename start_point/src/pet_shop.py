5b0b1e6cd1cb1f9cea54603660ba1dbd9bc22e79
Michael <admin@fillthegaplearning.co.uk>
2020-10-18 11:59:19 +0100

    last MVP committed and updated the 2nd last to work properly.

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -52,8 +52,15 @@ def remove_customer_cash(customer, cash):
     customer["cash"] -= cash
     return customer["cash"]
 
+
+##### Calls this one to check the count of pets #####
 def get_customer_pet_count(counts):
-    count = 0
-    for count in counts["pets"]:
-        count +=counts["pets"]
-    return count
+    counts = len(counts["pets"])
+    return counts
+
+# def get_customer_pet_count(customer):
+#     return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+    new_pet = len(customer["pets"])
